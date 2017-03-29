5c465e14a3d9e1d7db5b505ea1744524d5d8bebe
joaodefigueiredo <joaofigueiredo14@gmail.com>
2017-03-29 18:39:41 +0100

    User Type updated.

---
+++ src/public/js/main.js
@@ -0,0 +1,39 @@
+function showHide(elm) {
+  var phone = document.getElementById('phonenumber');
+  var email = document.getElementById('email');
+  if (elm.id == 'rdoPhone') {
+    phone.classList.remove('hide');
+    email.classList.add('hide');
+  } else
+  {
+    phone.classList.add('hide');
+    email.classList.remove('hide');
+  }
+}
+
+function fMember()   {
+  var member = document.getElementById('Member');
+  var partner = document.getElementById('Partner');
+  var costumer = document.getElementById('Costumer');
+  member.classList.remove('hide');
+  partner.classList.add('hide');
+  costumer.classList.add('hide');
+}
+
+function fPartner()   {
+  var member = document.getElementById('Member');
+  var partner = document.getElementById('Partner');
+  var costumer = document.getElementById('Costumer');
+  member.classList.add('hide');
+  partner.classList.remove('hide');
+  costumer.classList.add('hide');
+}
+
+function fCostumer()   {
+  var member = document.getElementById('Member');
+  var partner = document.getElementById('Partner');
+  var costumer = document.getElementById('Costumer');
+  member.classList.add('hide');
+  partner.classList.add('hide');
+  costumer.classList.remove('hide');
+}
