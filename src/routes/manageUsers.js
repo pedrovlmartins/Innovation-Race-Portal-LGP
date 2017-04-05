705b64eed7928ff3f4d7e2763c7db034a6383cab
EdgarDuarte14 <up201305973@fe.up.pt>
2017-04-05 17:16:48 +0100

    changing homepage to the new one

--- src/routes/manageUsers.js
+++ src/routes/manageUsers.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var database = require('database');
+
+//var database = require('database');
 
 router.get('/', function (req, res) {
   /*database.listAllUsers("token", funtion (err, result) {
