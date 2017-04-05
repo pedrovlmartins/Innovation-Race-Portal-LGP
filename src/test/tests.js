01e3becf0b60ac2b29a83967eb80c65efd542c55
Gustavo Silva <silva95gustavo@gmail.com>
2017-04-05 11:23:14 +0100

    Created simple form empty test.

--- src/test/tests.js
+++ src/test/tests.js
@@ -6,13 +6,32 @@ global.__base = __dirname + '/../';
 
 const path = require('path');
 var assert = require('assert');
+var chai = require('chai');
+var chaiHttp = require('chai-http');
 var database = require(path.join(__base, 'database', 'database'));
 var register = require(path.join(__base, 'routes', 'auth', 'register'));
+var server = require('../app.js');
+var should = chai.should();
+
+chai.use(chaiHttp);
 
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
       assert.equal(-1, [1, 2, 3].indexOf(4));
+    });
+  });
+});
+
+describe('Authentication', function () {
+  describe('Registration', function () {
+    it('should fail because the form is empty', function (done) {
+      chai.request(server)
+        .post('/auth/register')
+        .end(function (err, res) {
+          res.should.have.status(403);
+          done();
+        });
     });
   });
 });
