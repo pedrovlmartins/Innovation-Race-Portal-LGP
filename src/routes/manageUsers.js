96037e2a4049b5fce16543d15637bc4c95d5e88b
sceptross <up201304205@fe.up.pt>
2017-04-05 21:25:18 +0100

    Database corrections

--- src/routes/manageUsers.js
+++ src/routes/manageUsers.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
-//var database = require('database');
+var database = require('../database/database');
 
 router.get('/', function (req, res) {
   database.listAllUsers(function (result) {
