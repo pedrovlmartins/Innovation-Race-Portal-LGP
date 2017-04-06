bcc3b8760053a5dce74a8d42f790c465e22c6590
Gustavo Silva <silva95gustavo@gmail.com>
2017-04-06 01:18:31 +0100

    Fixed Travis build.

--- src/config.js
+++ src/config.js
@@ -5,19 +5,19 @@ config.env = process.env.NODE_ENV || 'development';
 config.mysql = {
   production: {
     connectionLimit: 10,
-    host: 'database',
+    host: 'localhost',
     user: 'root',
     database: 'irp',
   },
   development: {
     connectionLimit: 10,
-    host: 'database',
+    host: 'localhost',
     user: 'root',
     database: 'irp',
   },
   test: {
     connectionLimit: 10,
-    host: 'database',
+    host: 'localhost',
     user: 'root',
     database: 'irp_test',
   },
