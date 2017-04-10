a49daa092541d59ca91755a01cdf1ae4bcc28773
sceptross <up201304205@fe.up.pt>
2017-04-10 01:21:30 +0100

    Send 404 when accessing non-existent idea

--- src/routes/ideaPage.js
+++ src/routes/ideaPage.js
@@ -5,26 +5,30 @@ var db = require(path.join(__base, 'database', 'database'));
 
 router.get('/:id', function (req, res) {
   db.getIdea(req.params.id, function (ideaInfo) {
-    db.getTeamMembers(req.params.id, function (members) {
-      var ids = members.map((member) => {
-        member.id;
+    if (ideaInfo === undefined)
+      res.sendStatus(404);
+    else {
+      db.getTeamMembers(req.params.id, function (members) {
+        var ids = members.map((member) => {
+            member.id;
+        })
+        ids.push(ideaInfo.creatorId);
+        if (req.session !== undefined) {
+          if (ids.indexOf(req.session.userID) !== -1)
+            res.render('ideaPage', {
+              name: ideaInfo.name,
+              leader: ideaInfo.creator,
+              description: ideaInfo.description,
+              resultsToProduce: ideaInfo.resultsToProduce,
+              uncertaintyToSolve: ideaInfo.uncertaintyToSolve,
+              techHumanResources: ideaInfo.techHumanResources,
+              solutionTechnicalCompetence: ideaInfo.solutionTechnicalCompetence,
+              members: members,
+            });
+        } else
+          res.sendStatus(403);
       });
-      ids.push(ideaInfo.creatorId);
-      if (req.session !== undefined) {
-        if (ids.indexOf(req.session.userID) !== -1)
-          res.render('ideaPage', {
-            name: ideaInfo.name,
-            leader: ideaInfo.creator,
-            description: ideaInfo.description,
-            resultsToProduce: ideaInfo.resultsToProduce,
-            uncertaintyToSolve: ideaInfo.uncertaintyToSolve,
-            techHumanResources: ideaInfo.techHumanResources,
-            solutionTechnicalCompetence: ideaInfo.solutionTechnicalCompetence,
-            members: members,
-          });
-      } else
-        res.send(403);
-    });
+    }
   });
 });
 
