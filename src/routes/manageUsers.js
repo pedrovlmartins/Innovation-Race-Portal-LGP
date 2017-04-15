f21d6aec21f07d84c8728a77891f3f78a310af3a
EdgarDuarte14 <up201305973@fe.up.pt>
2017-04-15 21:50:28 +0100

    refactoring code in manage users

--- src/routes/manageUsers.js
+++ src/routes/manageUsers.js
@@ -25,28 +25,6 @@ router.get('/', function (req, res) {
       res.render('manageUsers', vars);
     });
   }
-
-  //checkar se recebo parametro no url (do metodo get -> keyword) terei de
-  // mostrar a tabela com os resultados filtrados
-
-  //res.render('manageUsers');
 });
-
-/*
-router.get('/manageUsers/search/process', function (req, res, next) {
-  var keyword = req.body.keyword;
-  console.log(keyword);
-  res.redirect('/search');
-});
-
-router.get('/search/:keyword', function (req, res) {
-  database.searchUsers(req.params.keyword, function (result) {
-    var users = result;
-    if (req.session.userID !== undefined)
-      vars.userID = req.session.userID;
-    res.render('manageUsers', vars);
-  });
-});
-*/
 
 module.exports = router;
