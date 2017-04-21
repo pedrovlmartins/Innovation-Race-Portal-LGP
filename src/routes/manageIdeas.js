3bc8a20a12c05b098d90c7a223d9c9d889c76ff1
sceptross <up201304205@fe.up.pt>
2017-04-21 01:39:20 +0100

    User permission check

--- src/routes/manageIdeas.js
+++ src/routes/manageIdeas.js
@@ -8,54 +8,60 @@ var ideas = require(path.join(__base, 'lib', 'ideas'));
 const itemsPerPage = 10;
 
 router.get('/', function (req, res) {
-  var vars = irp.getActionResults(req);
-  var keyword = req.query.keyword;
-  var offset;
-  var page;
+  database.getUserType(req.session.userID, function(type) {
+    if (type < 3) {
+      res.sendStatus(403);
+    } else {
+      var vars = irp.getActionResults(req);
+      var keyword = req.query.keyword;
+      var offset;
+      var page;
 
-  if (req.query.page === undefined) {
-    offset = 0;
-    page = 1;
-  } else {
-    page = parseInt(req.query.page);
-    if (isNaN(page)) {
-      offset = 0;
-      page = 1;
-    } else if (page < 1) {
-      offset = 0;
-      page = 1;
-    } else offset = (page - 1) * itemsPerPage;
-  }
+      if (req.query.page === undefined) {
+        offset = 0;
+        page = 1;
+      } else {
+        page = parseInt(req.query.page);
+        if (isNaN(page)) {
+          offset = 0;
+          page = 1;
+        } else if (page < 1) {
+          offset = 0;
+          page = 1;
+        } else offset = (page - 1) * itemsPerPage;
+      }
 
-  vars.page = page;
+      vars.page = page;
 
-  if (req.query.keyword === undefined) {
-    database.getIdeaCount(function (result) {
-      var numberOfIdeas = result[0].count;
-      vars.totalPages = Math.floor(numberOfIdeas / itemsPerPage);
-      if (numberOfIdeas % itemsPerPage > 0)
-        vars.totalPages += 1;
-      database.listIdeas(offset, itemsPerPage, function (result) {
-        result.forEach((idea) => idea.state = ideas.getStateName(idea.state));
-        vars.ideas = result;
-        if (req.session.userID !== undefined)
-          vars.userID = req.session.userID;
-        res.render('manageIdeas', vars);
-      });
-    });
-  } else {
-    database.searchIdeas(keyword, offset, itemsPerPage, function (error, result) {
-      var numberOfIdeas = result.length;
-      vars.keyword = keyword;
-      vars.totalPages = Math.floor(numberOfIdeas / itemsPerPage);
-      vars.ideas = result;
-      if (numberOfIdeas % itemsPerPage > 0)
-        vars.totalPages += 1;
-      if (req.session.userID !== undefined)
-        vars.userID = req.session.userID;
-      res.render('manageIdeas', vars);
-    });
-  }
+      if (req.query.keyword === undefined) {
+        database.getIdeaCount(function(result) {
+          var numberOfIdeas = result[0].count;
+          vars.totalPages = Math.floor(numberOfIdeas / itemsPerPage);
+          if (numberOfIdeas % itemsPerPage > 0)
+            vars.totalPages += 1;
+          database.listIdeas(offset, itemsPerPage, function(result) {
+            result.forEach((idea) => idea.state = ideas.getStateName(idea.state));
+            vars.ideas = result;
+            if (req.session.userID !== undefined)
+              vars.userID = req.session.userID;
+            res.render('manageIdeas', vars);
+          });
+        });
+      } else {
+        database.searchIdeas(keyword, offset, itemsPerPage, function(error, result) {
+          var numberOfIdeas = result.length;
+          vars.keyword = keyword;
+          vars.totalPages = Math.floor(numberOfIdeas / itemsPerPage);
+          vars.ideas = result;
+          if (numberOfIdeas % itemsPerPage > 0)
+            vars.totalPages += 1;
+          if (req.session.userID !== undefined)
+            vars.userID = req.session.userID;
+          res.render('manageIdeas', vars);
+        });
+      }
+    }
+  });
 });
 
 module.exports = router;
