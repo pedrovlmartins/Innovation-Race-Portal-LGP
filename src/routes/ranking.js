3499a6e4b15ca722f518472f12ce1d691efde3eb
EdgarDuarte14 <up201305973@fe.up.pt>
2017-04-29 21:23:32 +0100

    ranking page started

---
+++ src/routes/ranking.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const irp = require(path.join(__base, 'lib', 'irp'));
+var express = require('express');
+var router = express.Router();
+var database = require('../database/database');
+var ideas = require(path.join(__base, 'lib', 'ideas'));
+
+const itemsPerPage = 10;
+
+router.get('/', function (req, res) {
+  var vars = irp.getActionResults(req);
+  var offset;
+  var page;
+
+  if (req.query.page === undefined) {
+    offset = 0;
+    page = 1;
+  } else {
+    page = parseInt(req.query.page);
+    if (isNaN(page)) {
+      offset = 0;
+      page = 1;
+    } else if (page < 1) {
+      offset = 0;
+      page = 1;
+    } else offset = (page - 1) * itemsPerPage;
+  }
+
+  vars.page = page;
+
+  database.getIdeaCount(function (result) {
+    var numberOfIdeas = result[0].count;
+    vars.totalPages = Math.floor(numberOfIdeas / itemsPerPage);
+    if (numberOfIdeas % itemsPerPage > 0)
+      vars.totalPages += 1;
+    database.listIdeas(offset, itemsPerPage, function (result) {
+      result.forEach(
+        (idea) => idea.state = ideas.getStateName(idea.state, idea.cancelled)
+      );
+      vars.ideas = result;
+      if (req.session.userID !== undefined)
+        vars.userID = req.session.userID;
+      res.render('ranking', vars);
+      irp.cleanActionResults(req);
+    });
+  });
+});
+
+module.exports = router;
