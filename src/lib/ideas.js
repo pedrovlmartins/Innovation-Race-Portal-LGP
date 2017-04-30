813d42fdbf61c98a36320c43a9603b67ae1f03ea
José Pedro <josepedroteles@gmail.com>
2017-04-30 23:41:01 +0100

    revert
    about alterou ficheiros a mais

--- src/lib/ideas.js
+++ src/lib/ideas.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const email = require(path.join(__base, 'lib', 'mailer'));
 const EmailTemplate = require('email-templates').EmailTemplate;
-const evaluationNotificationEmailTemplateDir =
-  path.join(__base, 'views', 'emails', 'evaluationNotification');
+const selectionNotificationEmailTemplateDir =
+   path.join(__base, 'views', 'emails', 'selectionNotification');
 
 var states = {
   DRAFT: 0,
@@ -46,23 +46,6 @@ module.exports = {
     return true;
   },
 
-  sendEvaluationNotificationEmail: function (to, ideaName, approved, callback) {
-    var newsletter = new EmailTemplate(evaluationNotificationEmailTemplateDir);
-    var data = {
-      ideaName: ideaName,
-      approved: approved,
-      replyTo: 'altran@musaic.ml',
-    };
-    newsletter.render(data, function (err, result) {
-      if (err) console.error(err);
-      email.send(to,
-        'Innovation Race Portal - Your idea has been evaluated by the technical directors',
-        result.text, result.html,
-        function (error, body) {
-          callback(error, body);
-        });
-    });
-  },
 
   sendSelectionNotificationEmail: function (to, ideaName, selected, callback) {
     var newsletter = new EmailTemplate(selectionNotificationEmailTemplateDir);
@@ -73,9 +56,7 @@ module.exports = {
     };
     newsletter.render(data, function (err, result) {
       if (err) console.error(err);
-      email.send(to,
-        'Innovation Race Portal - Your idea has been analyzed by the committee',
-        result.text, result.html,
+      email.send(to, 'Innovation Race Portal - Your idea has been analyzed by the committee', result.text, result.html,
         function (error, body) {
           callback(error, body);
         });
