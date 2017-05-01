438c7387cd9f4091c781896c76911b1550a12060
sceptross <up201304205@fe.up.pt>
2017-05-01 14:12:24 +0100

    Sort user ideas alphabetically

--- src/database/database.js
+++ src/database/database.js
@@ -72,6 +72,7 @@ module.exports = {
             'SELECT ideas.id, ideas.title, ideas.state, ideas.cancelled, ideas.score ' +
             'FROM ideas ' +
             'WHERE ideas.idCreator = ? ' +
+            'ORDER BY ideas.title ' +
             'LIMIT ?, ?;', [id, limit, offset], function (err, result) {
                 if (typeof next === 'function')
                     next(result);
