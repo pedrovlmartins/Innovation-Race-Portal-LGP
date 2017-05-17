582226717a94e55c4fae10289bbc75c9f4ae29bf
José Pedro <josepedroteles@gmail.com>
2017-05-17 17:26:57 +0100

    Update db.sql

--- scripts/database/db.sql
+++ scripts/database/db.sql
@@ -19,7 +19,6 @@
 -- Table structure for table `ideamember`
 --
 
-LOCK TABLES `ideamember` WRITE;
 DROP TABLE IF EXISTS `ideamember`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
