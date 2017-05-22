f0824b3c7376f63acec66909ed1e8e52b22001b9
esquilofeup <esquilofeup@gmail.com>
2017-05-22 08:08:49 +0100

    Front-end before e-mail sending

---
+++ src/routes/passwordReset.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const irp = require(path.join(__base, 'lib', 'irp'));
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res) {
+  var vars = irp.getActionResults(req);
+  if (req.session.userID !== undefined)
+    vars.userID = req.session.userID;
+  res.render('passwordReset', vars);
+});
+
+module.exports = router;
