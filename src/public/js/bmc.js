7d8a66579f156237cc3926eb82316576a67dd882
esquilofeup <esquilofeup@gmail.com>
2017-05-27 00:46:12 +0100

    Textarea bug fixed

--- src/public/js/bmc.js
+++ src/public/js/bmc.js
@@ -2,3 +2,9 @@ $(document).ready(function() {
         $('[data-toggle="tooltip"]').tooltip();
     }
 );
+
+$(document).ready(function() {
+    $('#submitBMC').click(function () {
+                $('#submitBMC').attr('disabled', true);
+    }
+);}
