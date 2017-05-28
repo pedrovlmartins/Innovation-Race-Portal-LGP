c8ac114623bf80898eddcd1b305f098bcfa6720f
esquilofeup <esquilofeup@gmail.com>
2017-05-28 19:47:26 +0100

    Fixed BMC front-end

--- src/public/js/bmc.js
+++ src/public/js/bmc.js
@@ -2,9 +2,3 @@ $(document).ready(function() {
         $('[data-toggle="tooltip"]').tooltip();
     }
 );
-
-$(document).ready(function() {
-    $('#keyRevenueTextarea').mouseover(function() {
-        $('#keyRevenueTextarea').hide();
-    });
-}
